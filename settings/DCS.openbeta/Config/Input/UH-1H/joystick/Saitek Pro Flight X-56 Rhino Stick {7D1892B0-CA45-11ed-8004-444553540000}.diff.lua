08b65a7dccee9bf99a93b7759b2e4fbb49f25aa0
Andykan <andishe.kanani@gmail.com>
2024-06-25 14:39:14 +0200

    Add UH-1H and AJS37

---
+++ settings/DCS.openbeta/Config/Input/UH-1H/joystick/Saitek Pro Flight X-56 Rhino Stick {7D1892B0-CA45-11ed-8004-444553540000}.diff.lua
@@ -0,0 +1,55 @@
+local diff = {
+	["axisDiffs"] = {
+		["a2001cdnil"] = {
+			["changed"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0.15,
+						},
+						["deadzone"] = 0.01,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_Y",
+				},
+			},
+			["name"] = "Flight Control Cyclic Pitch",
+		},
+		["a2002cdnil"] = {
+			["changed"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0.15,
+						},
+						["deadzone"] = 0.01,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_X",
+				},
+			},
+			["name"] = "Flight Control Cyclic Roll",
+		},
+		["a2003cdnil"] = {
+			["name"] = "Flight Control Yaw (pedals)",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_RZ",
+				},
+			},
+		},
+	},
+}
+return diff
--- settings/DCS.openbeta/Config/pluginsEnabled.lua
+++ settings/DCS.openbeta/Config/pluginsEnabled.lua
@@ -1,4 +1,6 @@
 pluginsEnabled = 
 {
+    ["UH-1H Huey by Belsimtek"] = false,
+    ["F-16C bl.50"] = true,
     ["MiG-19P by RAZBAM"] = false,
 } -- end of pluginsEnabled
